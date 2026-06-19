245f5559a6a83b6aaee1b36a71f3d31929d59d18
Natchanon45 <103320884+Natchanon45@users.noreply.github.com>
2026-06-19 20:21:21 +0700

    Show price times quantity in receipt middle column

--- public/assets/js/receipt.js
+++ public/assets/js/receipt.js
@@ -52,10 +52,9 @@ async function render(order) {
 
   document.querySelector("#receiptItems").innerHTML = (order.items || []).map(item => `
     <tr>
-      <td>${item.name}${item.note ? `<div style="font-size:.85em">${item.note}</div>` : ""}</td>
-      <td class="num">${item.qty}</td>
-      <td class="num">${money(Number(item.price))}</td>
-      <td class="num">${money(Number(item.qty) * Number(item.price))}</td>
+      <td class="receipt-item-name">${item.name}${item.note ? `<div class="receipt-item-note">${item.note}</div>` : ""}</td>
+      <td class="num receipt-unit">${money(Number(item.price))} x ${item.qty}</td>
+      <td class="num receipt-line-total">${money(Number(item.qty) * Number(item.price))}</td>
     </tr>
   `).join("");
 
