916f93cac6fb567fa30c6a3f548d3dec09f8d60c
Natchanon45 <103320884+Natchanon45@users.noreply.github.com>
2026-06-21 08:16:02 +0700

    Notify kitchen about new Delivery orders

--- public/assets/js/kitchen.js
+++ public/assets/js/kitchen.js
@@ -1,5 +1,6 @@
 import { dataService, usingDemoMode } from "./data-service.js";
 import { money, statusLabel, formatTime, toast } from "./ui.js";
+import { observeDeliveryOrders } from "./delivery-notifier.js";
 
 if (usingDemoMode) document.querySelector("#demoBanner").innerHTML = '<div class="demo-banner">โหมดตัวอย่าง</div>';
 
@@ -103,4 +104,7 @@ grid.addEventListener("click", async event => {
   }
 });
 
-dataService.subscribeOrders(render);
+dataService.subscribeOrders(orders => {
+  observeDeliveryOrders(orders);
+  render(orders);
+});
